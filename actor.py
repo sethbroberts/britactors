5724d68220fa09a0cf4a86a5eaaa5ff8e3391247
Seth Roberts <seth.b.roberts@gmail.com>
2020-02-01 13:23:15 -0500

    app working, model added

---
+++ actor.py
@@ -0,0 +1,79 @@
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse, HTMLResponse, RedirectResponse
+from fastai.vision import (
+    ImageDataBunch,
+    cnn_learner,
+    open_image,
+    get_transforms,
+    models,
+    load_learner
+)
+import torch
+from pathlib import Path
+from io import BytesIO
+import sys
+import uvicorn
+import aiohttp
+import asyncio
+
+async def get_bytes(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.read()
+
+
+app = Starlette()
+
+path = ('.')
+learner = load_learner(path)
+
+@app.route("/upload", methods=["POST"])
+async def upload(request):
+    data = await request.form()
+    bytes = await (data["file"].read())
+    return predict_image_from_bytes(bytes)
+
+
+@app.route("/classify-url", methods=["GET"])
+async def classify_url(request):
+    bytes = await get_bytes(request.query_params["url"])
+    return predict_image_from_bytes(bytes)
+
+
+def predict_image_from_bytes(bytes):
+    img = open_image(BytesIO(bytes))
+    losses = learner.predict(img)
+    return JSONResponse({
+        "predictions": sorted(
+            zip(learner.data.classes, map(float, losses[2])),
+            key=lambda p: p[1],
+            reverse=True
+        )
+    })
+
+
+@app.route("/")
+def form(request):
+    return HTMLResponse(
+        """
+        <form action="/upload" method="post" enctype="multipart/form-data">
+            Select image to upload:
+            <input type="file" name="file">
+            <input type="submit" value="Upload Image">
+        </form>
+        Or submit a URL:
+        <form action="/classify-url" method="get">
+            <input type="url" name="url">
+            <input type="submit" value="Fetch and analyze image">
+        </form>
+    """)
+
+
+@app.route("/form")
+def redirect_to_homepage(request):
+    return RedirectResponse("/")
+
+
+if __name__ == "__main__":
+    if "serve" in sys.argv:
+        uvicorn.run(app, host="0.0.0.0", port=8008)
